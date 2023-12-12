0c048bbecc5767bb62148aead9f084ada5037aa1
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-12-12 21:34:13 +0300

    project

---
+++ HW(teacher)/Program.cs
@@ -0,0 +1,15 @@
+﻿mmusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_teacher_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ HW(teacher)/project 12.12.23/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_teacher_
+{
+    internal class Program
+    {
+        //11. Напишите программу, которая запрашивает у пользователя строку и проверяет,
+        //является ли она палиндромом(читается одинаково с начала и с конца).
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            for(int i = s.Length - 1; i >= 0; i--) 
+            {
+                if(s[i]string() != " ")
+            {
+
+                s += s[i]string();
+
+            }
+            }
+        }
+    }
+}
